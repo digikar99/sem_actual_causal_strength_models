a2b17da90bdd84d04de835b9cd5684b0f01a9abd
digikar <digikar@proton.me>
2025-12-31 13:18:51 +0100

    Add several CES scenarios provided by Tadeg et al

--- cesm_scenarios.py
+++ cesm_scenarios.py
@@ -1,5 +1,5 @@
 from counterfactual_effect_size_model import CESModel
-from sympy.abc import A, B, C, D
+from sympy.abc import A, B, C, D, E, R, U
 from sympy import Symbol, symbols
 
 poison, antidote, survival = symbols("poison, antidote, survival")
@@ -38,6 +38,7 @@ ql2023_exp2a_cesm = CESModel(
 		Win: (low + intermediate + high >= 2)
 	}
 )
+# compute_cesm_preds(ql2023_exp2a_cesm, 100000, [low, intermediate, high], Win, 0.7)
 
 # compare_cesm_scores(
 # 	ql2023_exp2a_cesm,
@@ -98,6 +99,7 @@ ql2023_exp3_two_cesm = CESModel(
 		Win: ((purple_low | purple_high) & orange)
 	}
 )
+# compute_cesm_preds(ql2023_exp3_two_cesm, 100000, [purple_low, orange], Win, 0.7)
 # compare_cesm_scores(
 # 	ql2023_exp3_two_cesm,
 # 	[purple_low, orange],
@@ -112,9 +114,126 @@ ql2023_exp3_three_cesm = CESModel(
 		Win: ((purple_low | purple_high) & orange)
 	}
 )
+# compute_cesm_preds(ql2023_exp3_three_cesm, 100000, [purple_low, purple_high, orange], Win, 0.7)
 # compare_cesm_scores(
 # 	ql2023_exp3_three_cesm,
 # 	[purple_low, purple_high, orange],
 # 	Win,
 # 	[0.2, 0.5, 0.8]
 # )
+
+top, left, right,  = symbols("top, left, right")
+ql2023_exp4_cesm = []
+for actuals in [{top, left, right, Win}, {top, left, Win}]:
+	for ptop in [0.1, 0.9]:
+		for pright in [0.1, 0.9]:
+			ql2023_exp4_cesm.append(
+				CESModel(
+					actuals = actuals,
+					exovar_probs = {top: ptop, left: 0.5, right: pright},
+					streq = {
+						Win: (top & left) | (~top & right)
+					}
+				)
+			)
+# for idx, model in enumerate(ql2023_exp4_cesm):
+# 	print(compute_cesm_preds(model, 100000, [top], Win, 0.7))
+
+# compute_cesm_preds(ql2023_exp4_cesm_09, 100000, [top], Win, 0.7)
+
+tadeg_example_1 = CESModel(
+	actuals = {A, B, C, E},
+	exovar_probs = {A: 0.1, B: 0.2, C: 0.4, D: 0.1},
+	streq = {
+		E: A & (B | C) & ~D
+	}
+)
+# compute_cesm_preds(tadeg_example_1, 100000, [A], E, 0.7)
+
+tadeg_example_2 = [
+	CESModel(
+		actuals = {A, B, E},
+		exovar_probs = {A: 0.1, B: 0.9},
+		streq = {
+			E: A & B
+		}
+	),
+	CESModel(
+		actuals = {A, B, E},
+		exovar_probs = {A: 0.1, B: 0.9},
+		streq = {
+			E: A | B
+		}
+	)
+]
+# compute_cesm_preds(tadeg_example_2[0], 100000, [A, B], E, 0.7)
+# compute_cesm_preds(tadeg_example_2[1], 100000, [A, B], E, 0.7)
+
+gi2020_xor_cesm = [
+	CESModel(
+		actuals = {A, B, E},
+		exovar_probs = {A: 0.8, B: 0.2},
+		streq = {
+			E: (A & ~B) | (~A & B)
+		}
+	),
+	CESModel(
+		actuals = set(),
+		exovar_probs = {A: 0.8, B: 0.2},
+		streq = {
+			E: (A & ~B) | (~A & B)
+		}
+	)
+]
+# compute_cesm_preds(gi2020_xor_cesm[0], 100000, [A, B], E, 0.7)
+# compute_cesm_preds(gi2020_xor_cesm[1], 100000, [A, B], E, 0.7)
+
+oneill2025_dp_cesm = CESModel(
+	actuals = {E, U, C, D},
+	exovar_probs = {C:0.5, U: 0.1, D: 0.5},
+	streq = {
+		E: C & ~R,
+		R: U & ~D
+	}
+)
+# compute_cesm_preds(oneill2025_dp_cesm, 100000, [C, D], E, 0.7)
+
+confounded_cesm = CESModel(
+	actuals = {A, C, E},
+	exovar_probs = {A: 0.9},
+	streq = {
+		E: A,
+		C: A
+	}
+)
+# compute_cesm_preds(confounded_cesm, 100000, [C], E, 0.7)
+
+nonconfounded_cesm = CESModel(
+	actuals = {A, C, E},
+	exovar_probs = {A: 0.3, C: 0.6},
+	streq = {
+		E: A & C
+	}
+)
+# compute_cesm_preds(nonconfounded_cesm, 100000, [C], E, 0.7)
+# compute_cesm_preds(nonconfounded_cesm, 100000, [E], C, 0.7)
+
+intconf_one_cesm = CESModel(
+	actuals = {A, C, E},
+	exovar_probs = {A: 0.5, B: 0.5},
+	streq = {
+		E: A | C,
+		C: A | B
+	}
+)
+# compute_cesm_preds(intconf_one_cesm, 100000, [A, C], E, 0.7)
+
+intconf_two_cesm = CESModel(
+	actuals = {A, B, C, E},
+	exovar_probs = {A: 0.5, B: 0.5},
+	streq = {
+		E: A | C,
+		C: A | B
+	}
+)
+# compute_cesm_preds(intconf_two_cesm, 100000, [A, C], E, 0.7)
